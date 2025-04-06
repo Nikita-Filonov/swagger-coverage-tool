3a8e3d9180258a7daba113ce5eeecc6990faf3e9
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-06 21:18:06 +0300

    Update models.py

--- swagger_coverage_tool/src/libs/models.py
+++ swagger_coverage_tool/src/libs/models.py
@@ -1,12 +1,16 @@
+from http import HTTPMethod
 from typing import Self
 
 from pydantic import BaseModel, RootModel
 
+from swagger_coverage_tool.src.tools.types import ServiceKey, StatusCode, EndpointName
+
 
 class EndpointCoverage(BaseModel):
-    name: str
-    method: str
-    status_code: int
+    name: EndpointName
+    method: HTTPMethod
+    service: ServiceKey
+    status_code: StatusCode
 
 
 class EndpointCoverageList(RootModel):
@@ -14,15 +18,17 @@ class EndpointCoverageList(RootModel):
 
     def filter(
             self,
-            name: str | None = None,
-            method: str | None = None,
-            status_code: int | None = None
+            name: EndpointName | None = None,
+            method: HTTPMethod | None = None,
+            service: ServiceKey | None = None,
+            status_code: StatusCode | None = None
     ) -> Self:
         results = [
             coverage
             for coverage in self.root
             if (name is None or coverage.name.lower() == name.lower()) and
                (method is None or coverage.method.lower() == method.lower()) and
+               (service is None or coverage.service.lower() == service.lower()) and
                (status_code is None or coverage.status_code == status_code)
         ]
         return EndpointCoverageList(root=results)
