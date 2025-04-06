dd68fe8e423b7672725dab3848c3a8097c30abde
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-06 16:24:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="swagger-coverage-tool",
-    version="0.1.0",
+    version="0.3.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "click",
         "httpx",
