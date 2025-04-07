acf682e39b7c540f271ad859ba65eb47fe22bc08
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-07 21:35:10 +0300

    Update __init__.py

--- swagger_coverage_tool/__init__.py
+++ swagger_coverage_tool/__init__.py
@@ -1,4 +1,3 @@
-from swagger_coverage_tool.config import get_settings
 from swagger_coverage_tool.src.libs.tracker import SwaggerCoverageTracker
 
-__all__ = ["get_settings", "SwaggerCoverageTracker"]
+__all__ = ["SwaggerCoverageTracker"]
