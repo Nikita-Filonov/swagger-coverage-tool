a51eb45991d87e90b900699fa3a58be593a5163a
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-09 19:45:26 +0300

    Update models.py

--- swagger_coverage_tool/src/coverage/models.py
+++ swagger_coverage_tool/src/coverage/models.py
@@ -3,16 +3,24 @@ from pydantic import BaseModel, computed_field, Field, ConfigDict
 from swagger_coverage_tool.src.history.models import CoverageHistory
 from swagger_coverage_tool.src.tools.http import HTTPMethod
 from swagger_coverage_tool.src.tools.percent import get_coverage_percent
-from swagger_coverage_tool.src.tools.types import StatusCode, EndpointName, CoveragePercent
+from swagger_coverage_tool.src.tools.types import StatusCode, EndpointName, CoveragePercent, QueryParameter
 
 
 class ServiceEndpointStatusCodeCoverage(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
     value: StatusCode
-    covered: bool
+    is_covered: bool = Field(alias="isCovered")
     total_cases: int = Field(alias="totalCases")
     description: str | None = None
+    is_response_covered: bool = Field(alias="isResponseCovered")
+
+
+class ServiceEndpointQueryParametersCoverage(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+    name: QueryParameter
+    is_covered: bool = Field(alias="isCovered")
 
 
 class ServiceEndpointCoverage(BaseModel):
@@ -21,9 +29,11 @@ class ServiceEndpointCoverage(BaseModel):
     name: EndpointName
     method: HTTPMethod
     summary: str | None = None
-    covered: bool
+    is_covered: bool = Field(alias="isCovered")
     total_cases: int = Field(alias="totalCases")
     status_codes: list[ServiceEndpointStatusCodeCoverage] = Field(alias="statusCodes")
+    query_parameters: list[ServiceEndpointQueryParametersCoverage] = Field(alias="queryParameters")
+    is_request_covered: bool = Field(alias="isRequestCovered")
     total_coverage_history: list[CoverageHistory] = Field(alias="totalCoverageHistory", default_factory=list)
 
     @computed_field(alias="totalCoverage")
@@ -33,7 +43,7 @@ class ServiceEndpointCoverage(BaseModel):
         if not total:
             return CoveragePercent(0.0)
 
-        covered = len(list(filter(lambda e: e.covered, self.status_codes)))
+        covered = len(list(filter(lambda e: e.is_covered, self.status_codes)))
         return get_coverage_percent(total=total, covered=covered)
 
 
@@ -52,5 +62,5 @@ class ServiceCoverage(BaseModel):
         if not total:
             return CoveragePercent(0.0)
 
-        covered = len(list(filter(lambda e: e.covered, self.endpoints)))
+        covered = len(list(filter(lambda e: e.is_covered, self.endpoints)))
         return get_coverage_percent(total=total, covered=covered)
