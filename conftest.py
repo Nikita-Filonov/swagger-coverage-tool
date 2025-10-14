c1c77caa86eacd7d05769874e903f57b8d68336a
Nikita Filonov <nikita.filonov@dif.tech>
2025-10-14 21:12:47 +0300

    tests

--- conftest.py
+++ conftest.py
@@ -4,4 +4,5 @@ pytest_plugins = (
     "swagger_coverage_tool.tests.fixtures.reports",
     "swagger_coverage_tool.tests.fixtures.history",
     "swagger_coverage_tool.tests.fixtures.tracker",
+    "swagger_coverage_tool.tests.fixtures.coverage",
 )
